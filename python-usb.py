478c0265ee8662a8bd3114106e906166271ea71f
Abtin <shervinmoheban@gmail.com>
2017-03-15 19:32:38 +0330

    Update python-usb.py

--- python-usb.py
+++ python-usb.py
@@ -1,17 +1,18 @@
+#import things :)
 from time import sleep
 import os
 from os.path import exists
 from shutil import copy2
 
-f=open('path.txt','r')
+f=open('path.txt','r') #reading from path.txt
 list = f.readlines()
-path = list[0].rstrip()
+path = list[0].rstrip() #removes \n from string
 dst = list[1].rstrip()
 f.close()
 while (True):
     sleep(5)
-    if exists(path):
-        for path , dirs ,files in os.walk(path):
+    if exists(path): # loking for src path 
+        for path , dirs ,files in os.walk(path): # searchs the src recursively
             for name in files :
                 if name.endswith((".pdf",".docx")):
                     print(name)
